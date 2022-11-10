1edb6b0e23d7d348eb259166ea8c60ebc721ecde
arXaik <araik200414@gmail.com>
2022-11-10 02:07:07 +0200

    Добавьте файлы проекта.

---
+++ lab 2.1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Петросян Араик 22-ИСП-2/1  вар 2,1
+Console.WriteLine("Введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите y:");
+double y = double.Parse(Console.ReadLine());
+double part1 = (x * x + y * y);
+double part2 = ((x - 2) * (x - 2) + y * y);
+if (part1 <= 1*1 && part2 <= 2 * 2 ) Console.WriteLine("Принадлежит");
+else Console.WriteLine("Не принадлежит");
+
